72dbfd995614983bc646db70da569a3ddbb3b75b
김민주 <kimmj476@ajou.ac.kr>
2025-08-18 16:36:31 +0900

    구글 api키 삭제

--- SW/Chain1/google_model.py
+++ SW/Chain1/google_model.py
@@ -1,7 +1,7 @@
 from google import genai
 
 # 🔑 API 키 환경변수에 설정해 두었으면 생략 가능
-client = genai.Client(api_key="AIzaSyADmicI_5Fz92zn78h5ckHU-PwmLQPhSac")
+client = genai.Client(api_key="")
 
 # 지원되는 모델 리스트 확인
 models = client.models.list()
